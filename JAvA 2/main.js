d92116bf6c57f19e46ed5dc44f6493d35f2b0c7a
Muslimah-001 <fareedahadunwolemimustapha@gmail.com>
2026-04-08 19:32:36 -0500

    fifth commit

---
+++ JAvA 2/main.js
@@ -0,0 +1,24 @@
+{/* Promt:
+    10 people have entered friday
+    15 people entered monday
+    27 people entered tuesday
+    12 people entered wednesday
+    25 people entered thursday */}
+let monday = 10
+let  tuesday=27
+let wednesday = 12
+let  thursday= 25
+let friday = 10
+console.log( 'Entries Today : '  + 'Today: Friday/14th/2026 ' +  friday,)
+    console.log(' Previous Entries In The Last 4 Days; '  + ' Monday:' +  monday )
+     console.log(' Tuesday: ' + tuesday  ) 
+     console.log(' Wednesday :' + wednesday  )
+     console.log(' Thursday :' + thursday   )
+
+     var num1=10
+     var num2=27
+     var num3=12
+     var num4=25
+     var num5=10
+     var total =num1 + num2 + num3 + num4 + num5 ;
+console.log(total + ' People In Total In The Past 4 Days' + ' Log Book Complete.')
---
+++ JAvA/main.js
@@ -0,0 +1,30 @@
+// string concatenation
+
+let name;
+name ='Muslimah' ;
+
+let country = 'Canada';
+
+let amount = 50
+
+console.log('my name is ' + name, 'I am From ' + country  +  'I only have $' + amount + 'on me')
+
+
+// 1.Create 4 Variables using let key words , save these words inside
+//  -js programming,
+// - foundation lvl, 
+// -innerpeace programming
+
+// 2. log out this statement "My name is ......,
+//  I am 'age',
+//  I am in .......... 
+// at ............... .
+//  I am currently 
+// learning 
+// js programming launguage."
+
+let age = 10
+let place = 'Foundation Level'
+let program = 'Innerpeace Foundation'
+let launguage = 'Launguage'
+console.log('My name is ' + name, 'I am ' + age + ' I am in ' + place + ' at ' + program +  ' I am Currently Learning JS ' + launguage )
