810ee417b7407ef6c92a09a6ba62dbb5719d53ac
Muslimah-001 <fareedahadunwolemimustapha@gmail.com>
2026-05-09 15:44:23 -0500

    update

--- JAvA 3/main.js
+++ JAvA 3/main.js
@@ -102,17 +102,16 @@ console.log(numberCount)
 
 let numberspace = 0;
 function increment (){
-    numberspace = numberspace + 1
-    countEl.innerHTML = numberspace
+    numberspace = numberCount + 1
+    countEl.innerHTML = numberCount
 }
-let countEl = document.getElementById("counter")
 let saveEl = document.getElementById ("save-el")
 
 
 
 function save () {
     // console.log(numberspace)
-    let saveStr = numberspace + " - "
+    let saveStr = numberCount+ " - "
     
     saveEl.innerText += saveStr
 }
